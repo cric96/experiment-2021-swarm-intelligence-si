d09789b294d7393431402215d21fd2e642ce2534
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-12-05 10:28:51 +0100

    update temperature field (using a mixture of gaussian), update simulation effects

--- src/main/scala/it/casestudy/ClusteringLib.scala
+++ src/main/scala/it/casestudy/ClusteringLib.scala
@@ -3,4 +3,5 @@ package it.casestudy
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 trait ClusteringLib {
   self : AggregateProgram =>
+
 }
--- src/main/scala/it/unibo/alchemist/model/implementation/layers/LayerAggregator.java
+++ src/main/scala/it/unibo/alchemist/model/implementation/layers/LayerAggregator.java
@@ -4,6 +4,10 @@ import it.unibo.alchemist.model.interfaces.Environment;
 import it.unibo.alchemist.model.interfaces.Layer;
 import it.unibo.alchemist.model.interfaces.Position;
 
+/**
+ * A layer helper that merge all layers value into one molecule
+ * @param <P> the environment position
+ */
 public class LayerAggregator<P extends Position<P>> implements Layer<Double, P> {
     private final Environment<Double, P> env;
     public LayerAggregator(Environment<Double, P> env) {
