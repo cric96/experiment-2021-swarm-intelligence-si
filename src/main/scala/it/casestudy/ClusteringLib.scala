c6e2cab500e01c17d8efac72f4c32d474f0d2d01
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-12-11 17:11:29 +0100

    on cluster api

--- src/main/scala/it/casestudy/ClusteringLib.scala
+++ src/main/scala/it/casestudy/ClusteringLib.scala
@@ -3,12 +3,12 @@ package it.casestudy
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 // TODO move the implementations details here and expose a clear API for clusters formation
 trait ClusteringLib {
-  self: AggregateProgram with StandardSensors with BlockG =>
+  self: AggregateProgram with StandardSensors with BlockG with CustomSpawn =>
 
   /**
-   * Creates disjoint clusters identified by the ID of the leader node
-   * @param clusterCandidate a Boolean field that defines if a node is a cluster candidate (i.e. the center of the cluster) or nnot
-   * @param clusterField the data necessary to build the cluster (i.e. a temperature field, the potential, ...)
+   * Creates disjoint clusters identified by the ID of the candidate node
+   * @param clusterCandidate a Boolean field that defines if a node is a cluster candidate (i.e. the center of the cluster) or not
+   * @param clusterField the data necessary to build the cluster (i.e. a temperature field, ...)
    * @param fieldAccumulator define how the candidate value spread into the other nodes (you can use identity)
    * @param inCondition define the condition to join in the cluster
    * @param metric the metric used for the gradient cast
@@ -32,9 +32,50 @@ trait ClusteringLib {
   }
 
   /**
+   * Creates cluster (overlapped) identified by the ID of the candidate node
+   * @param clusterCandidate a Boolean field that defines if a node is a cluster candidate (i.e. the center of the cluster) or nnot
+   * @param clusterField the data necessary to build the cluster (i.e. a temperature field, the potential, ...)
+   * @param inCondition define the condition to join in to a particular cluster (id is the cluster ID)
+   * @param closeCondition define when a cluster is no longer active (e.g. some underline distribution condition are changed)
+   * @tparam V the field type
+   * @return a Map of ID -> V where ID is the cluster ID and V is the value of the central node
+   */
+  def cluster[V](
+    clusterCandidate: Boolean,
+    clusterField: V,
+    inCondition: ID => V => Boolean,
+    closeCondition: ID => V => Boolean
+  ): Map[ID, V] = {
+    val spawnLogic: ((ID, V)) => Unit => POut[V] = { case (id, value) =>
+      _ => {
+        // this condition is used to change leader.
+        // if it is leader (leader == mid()) and it is not a candidate anymore, it close the process.
+        mux(closeCondition(id)(value)) {
+          POut(value, SpawnInterface.Terminated)
+        } {
+          val status = if (inCondition(id)(value)) { SpawnInterface.Output }
+          else { SpawnInterface.External }
+          POut(value, status)
+        }
+      }
+    }
+    sspawn2[(ID, V), Unit, V](
+      spawnLogic,
+      mux(clusterCandidate) { Set((mid(), clusterField)) } { Set.empty[(ID, V)] },
+      args = {}
+    )
+      .map { case ((k, _), v) => k -> v }
+  }
+
+  /**
    * Facades for cluster identification
    */
   object cluster {
+
+    /**
+     * overlapped cluster identification
+     */
+    object overlapped {}
 
     /**
      * disjoint cluster identification
